528efa18e372f31ffd0804b5d54f11949e277641
Elina <88678660+fyodiya@users.noreply.github.com>
2022-08-26 10:27:01 +0300

    loading the stock from src file

--- src/main/scala/Task.scala
+++ src/main/scala/Task.scala
@@ -3,7 +3,17 @@ object Task extends App {
   val spark = Utilities.SparkUtilities.getOrCreateSpark("spark")
 
   //load the stock
-  //Compute the average daily return of every stock for every date
+
+  val src = "src/main/scala/resources/stock_prices.csv"
+  val df = spark.read
+    .format("csv")
+    .option("header", value = true)
+    .option("inferSchema", value = true)
+    .load(src)
+
+  df.show(10, truncate = false)
+
+  //compute the average daily return of every stock for every date
 
 
 }
