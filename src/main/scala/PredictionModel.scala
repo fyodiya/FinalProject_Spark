840ef8ae98610431707c4dfa524435111e1a18c9
Elina <88678660+fyodiya@users.noreply.github.com>
2022-08-29 12:31:27 +0300

    completed bonus task

--- src/main/scala/PredictionModel.scala
+++ src/main/scala/PredictionModel.scala
@@ -1,19 +1,18 @@
 import Utilities.SparkUtilities
-import org.apache.spark.ml.Pipeline
-import org.apache.spark.ml.feature.{StringIndexer, OneHotEncoder}
+import org.apache.spark.ml.{Pipeline, PipelineModel}
+import org.apache.spark.ml.classification.{LogisticRegression, LogisticRegressionModel}
+import org.apache.spark.ml.feature.{OneHotEncoder, RFormula, StringIndexer, VectorAssembler}
 import org.apache.spark.ml.evaluation.RegressionEvaluator
-import org.apache.spark.ml.feature.{VectorAssembler}
 import org.apache.spark.ml.regression.LinearRegression
-import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
+import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder, TrainValidationSplit, TrainValidationSplitModel}
 import org.apache.spark.sql.functions.{col, expr, round, to_date}
 
 object PredictionModel extends App {
 
   val spark = SparkUtilities.getOrCreateSpark("Spark")
-
   val src = "src/scala/resources/src/stock_prices.csv"
-  //Build a model either trying to predict the next day's price (regression)
-  // or simple UP/DOWN/UNCHANGED? classificator.
+
+  //TASK: Build a model trying to predict the next day's price (regression)
   //You can only use information information from earlier dates.
 
   val df = spark.read
@@ -26,28 +25,13 @@ object PredictionModel extends App {
   val dfWithReturn = df
     .withColumn("dailyReturn", dailyReturn)
     .orderBy("date")
-//    .select("date", "ticker", "dailyReturn")
 
   dfWithReturn.printSchema()
-
-//  val dfForProcessing = dfWithReturn
-////    .over(Window.partitionBy("ticker").orderBy("date"))
-//    .withColumn("date", col("date").cast("Double"))
-//    .withColumn("ticker", col("ticker").cast("Double"))
-//    .withColumn("close", col("close").cast("Double"))
-//    .withColumn("open", col("close").cast("Double"))
-//    .withColumn("dailyReturn", col("dailyReturn").cast("Double"))
-
-//dfForProcessing.createOrReplaceTempView("dfForProcessing")
-
-  //takes as input a number of columns of Boolean, Double, or Vector
-  //indexing strings for VectorAssembler
 
   val dateIndexer = new StringIndexer()
     .setInputCol("date")
     .setOutputCol("dateInd")
-//  dateIndexer.fit(dfWithReturn)
-//      .transform(dfWithReturn)
+
   val dfIndDate = dateIndexer.fit(dfWithReturn).transform(dfWithReturn)
   val dfForProcessing = dfIndDate
 
@@ -55,26 +39,40 @@ object PredictionModel extends App {
     .setInputCol("dateInd")
     .setOutputCol("dateEnc")
 
-//TODO "ticker" also should be indexed
-
+  val supervised = new RFormula()
+    .setFormula("close ~ . ")
+  val fittedDF = supervised.fit(dfForProcessing)
+  val preparedDF = fittedDF.transform(dfForProcessing)
+  preparedDF.show()
+  val Array(train, test) = preparedDF.randomSplit(Array(0.7, 0.3))
 
   val va = new VectorAssembler()
-    .setInputCols(Array("dateInd", "open", "close", "dailyReturn"))
+    .setInputCols(Array("dateInd", "open", "close", "high", "low", "dailyReturn"))
     .setOutputCol("features")
 
   val linReg = new LinearRegression()
     .setFeaturesCol("features")
     .setLabelCol("close") //since we try to predict the closing price
 
-  val pipeline = new Pipeline()
-    .setStages(Array(va, linReg))
+  val fittedLR = linReg.fit(train)
+  fittedLR.transform(train).select("label", "prediction")
+
+  val rForm = new RFormula()
 
   val params = new ParamGridBuilder()
-    .addGrid(linReg.regParam, Array(0, 0.5, 1))
+//    .addGrid(rForm.formula, Array(
+//      "close ~ . "
+//    ))
+    .addGrid(linReg.elasticNetParam, Array(0, 0.5, 1))
+    .addGrid(linReg.regParam, Array(0.1, 0.2))
     .build()
 
+  val pipeline = new Pipeline()
+    .setStages(Array(va, linReg))
+  //.setStages(Array(va, ohe, dateIndexer, linReg))
+
   val evaluator = new RegressionEvaluator()
-    .setMetricName("rmse")
+//    .setMetricName("rmse")
     .setPredictionCol("prediction")
     .setLabelCol("close")
 
@@ -84,9 +82,9 @@ object PredictionModel extends App {
     .setEstimatorParamMaps(params)
     .setNumFolds(2) //as advised by the book, may be changed to 2
 
-  val model = pipeline.fit(dfForProcessing)
-  val prediction = model.transform(dfForProcessing)
-
-  prediction.show()
+  val modelLR = cv.fit(dfForProcessing)
+  val predictionLR = modelLR.transform(dfForProcessing)
+//
+  predictionLR.show()
 
 }
